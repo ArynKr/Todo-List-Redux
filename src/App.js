25613df17a496f499142bc4665963accc9ce0f98
Aryan Kumar <aryankumar1504@gmail.com>
2021-02-19 19:29:00 +0530

    Basic Styling Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
 import Input from "./components/Input";
+import TodoItem from "./components/TodoItem";
+
+const todoList = [{ item: "todo", done: false, id: 23232 }];
 
 function App() {
   return (
     <div className="App">
       <div className="app_container"></div>
-
+      {todoList.map((item) => (
+        <TodoItem name={item.item} done={item.done} id={item.id} />
+      ))}
       <Input />
     </div>
   );
---
+++ src/components/TodoItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./TodoItem";
+
+import Checkbox from "@material-ui/core";
+
+const TodoItem = ({ name, done, id }) => {
+  const handleCheck = () => {};
+
+  return (
+    <div className="todoItem">
+      <Checkbox
+        checked={done}
+        color="primary"
+        onChange={handleCheck}
+        inputProps={{ "aria-label": "secondary checkbox" }}
+      />
+
+      <p className={done && "todoItem--done"}>{name}</p>
+    </div>
+  );
+};
+
+export default TodoItem;
